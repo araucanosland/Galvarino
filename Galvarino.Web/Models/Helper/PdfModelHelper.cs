b32f02103400030911020a6da88dc27e23e9dfc2
Carlos Pradenas <carlpradenas@gmail.com>
2018-10-29 12:19:22 -0300

    Pruebas de Pdf en Linux

---
+++ Galvarino.Web/Models/Helper/PdfModelHelper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Galvarino.Web.Models.Helper
+{
+    public class PdfModelHelper
+    {
+        public string FechaImpresion { get; set; }
+        public string CodigoSeguimiento { get; set; }
+    }
+}
