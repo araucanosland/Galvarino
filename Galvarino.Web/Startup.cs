c3c10d06987d6fd641b420af72a08338011be6c5
Carlos Pradenas <carlpradenas@gmail.com>
2019-09-02 16:12:10 -0400

    Se quita servicio con problemas *servicio de corrector de carga inicial, el cual se incluira en la nueva carga inicial remodelada

--- Galvarino.Web/Startup.cs
+++ Galvarino.Web/Startup.cs
@@ -96,7 +96,7 @@ namespace Galvarino.Web
             services.AddHostedService<CargaDatosCreditoService>();
             services.AddHostedService<GeneraArchivoIronMountain>();
             services.AddHostedService<CierrePagaresDeIronMountainWorker>();
-            services.AddHostedService<CorrectorCargaInicialWorker>();
+            //services.AddHostedService<CorrectorCargaInicialWorker>();
 
             
 
