28e4d119ab837e3d13e7c8ea5ba9cece60bede3c
Victor Arroyo <victor.arroyo.n@gmail.com>
2022-12-02 12:54:18 -0300

    worked

--- Galvarino.Web/Workers/GenerarReporteGestion.cs
+++ Galvarino.Web/Workers/GenerarReporteGestion.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Galvarino.Web.Services.Notification;
+using Galvarino.Web.Services.Workflow;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,57 +12,75 @@ using System.Threading.Tasks;
 
 namespace Galvarino.Web.Workers
 {
-    internal class GenerarReporteGestion : IHostedService, IDisposable
+    internal class GenerarReporteGestion : BackgroundService
     {
         private readonly ILogger _logger;
-        private Timer _timer;
-        private readonly TimeSpan horaInicial = new TimeSpan(22, 0, 0);
-        private readonly TimeSpan horaFinal = new TimeSpan(23, 59, 59);
-
         private readonly IConfiguration _configuration;
+        private readonly INotificationKernel _mailService;
+        private readonly IServiceScope _scope;
+        private Timer _timer;
         private bool estaOcupado = false;
+        private TimeSpan horaInicial;
+        private TimeSpan horaFinal;
 
-        public GenerarReporteGestion(ILogger<GenerarReporteGestion> logger, Timer timer, IConfiguration configuration)
+
+        public GenerarReporteGestion(ILogger<GenerarReporteGestion> logger, IServiceProvider services, IConfiguration configuration, INotificationKernel mailService)
         {
             _logger = logger;
-            _timer = timer;
             _configuration = configuration;
-
+            _mailService = mailService;
+            _scope = services.CreateScope();
+            this.setHora();
         }
 
-        public void Dispose()
-        {
-            _timer?.Dispose();
-        }
+   
 
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Timed Background Service is starting.");
-            object state = null;
-            _timer = new Timer(DoWork, state, TimeSpan.Zero, TimeSpan.FromMinutes(10));
-            return Task.CompletedTask;
-        }
-        public Task StopAsync(CancellationToken cancellationToken)
+        public override void Dispose() => _timer?.Dispose();
+
+        public override Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Timed Background Service is stopping.");
             _timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
         }
 
-       
-
         private void DoWork(object state)
         {
             var horaActual = DateTime.Now.TimeOfDay;
             if (horaActual >= horaInicial && horaActual <= horaFinal && !estaOcupado)
             {
-                estaOcupado = true;
-
 
             }
 
         }
 
-        
+
+
+        private void setHora()
+        {
+            var rawMomentoInicio = _configuration.GetValue<string>("CoordinacionWorkers:ReporteGetionWorker:HoraInicio");
+            int hInicio = Convert.ToInt32(rawMomentoInicio.Split(':')[0]);
+            int mInicio = Convert.ToInt32(rawMomentoInicio.Split(':')[1]);
+            int sInicio = Convert.ToInt32(rawMomentoInicio.Split(':')[2]);
+
+            this.horaInicial = new TimeSpan(hInicio, mInicio, sInicio);
+
+            var rawMomentoFin = _configuration.GetValue<string>("CoordinacionWorkers:ReporteGetionWorker:HoraFin");
+            int hFin = Convert.ToInt32(rawMomentoFin.Split(':')[0]);
+            int mFin = Convert.ToInt32(rawMomentoFin.Split(':')[1]);
+            int sFin = Convert.ToInt32(rawMomentoFin.Split(':')[2]);
+
+            this.horaFinal = new TimeSpan(hFin, mFin, sFin);
+
+        }
+
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Background Service is starting.");
+            object state = null;
+            _timer = new Timer(DoWork, state, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+            return Task.CompletedTask;
+        }
     }
 }
