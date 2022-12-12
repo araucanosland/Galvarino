c43919703a4e711e93b74c94e5b3391bfe7d18fc
rafaverdejo <rverdejog@laaraucana.cl>
2022-12-12 14:43:01 -0300

    menu reporte

--- Galvarino.Web/Startup.cs
+++ Galvarino.Web/Startup.cs
@@ -93,7 +93,7 @@ namespace Galvarino.Web
             //services.AddHostedService<CargaInicialWorker>();
             //services.AddHostedService<GeneraArchivoIronMountain>();
             
-            services.AddHostedService<GenerarReporteGestion>();
+            //services.AddHostedService<GenerarReporteGestion>();
 
             // services.AddHostedService<CargaDatosCreditoService>();
             //services.AddHostedService<CierrePagaresDeIronMountainWorker>();
